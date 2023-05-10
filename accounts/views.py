e6c115db2c97a09e1e8f5a0a993e7cd1f6d651cf
jisoo-lenovo <jisoo@envisageworldwide.com>
2023-05-10 17:35:44 +0700

    redirect to dashboard after logged in

--- accounts/views.py
+++ accounts/views.py
@@ -7,13 +7,17 @@ def home(request):
     return render(request, "home.html")
 
 
+def dashboard(request):
+    return render(request, "dashboards/dashboard.html")
+
+
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("home")
+            return redirect("dashboard")
     else:
         form = AuthenticationForm()
     return render(request, "accounts/login.html", {"form": form})
